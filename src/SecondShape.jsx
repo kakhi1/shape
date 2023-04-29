539238d6c28fdb1260b213d2e22fb6d5815e7c68
kakhi1 <kmchedluri@gmail.com>
2023-04-29 18:19:12 +0400

    add coment

--- src/SecondShape.jsx
+++ src/SecondShape.jsx
@@ -4,11 +4,13 @@ const SecondShape = () => {
   return (
     <div className="w-full md:h-[850px] flex justify-center items-center  bg-[#2d3e49] ">
       <div className="max-w-[800px] max-h-[800px] relative  bg-[#2d3e49] m-6 ">
+        {/* disconect button */}
         <div className="z-30 text-white -top-4 left-[35%] absolute">
           <h1 className="lg:text-2xl sm:text-xl text-xs font-semibold uppercase">
             discconect
           </h1>
         </div>
+        {/* start shape code  */}
         <div className=" h-[100%] w-[100%] bg-[#43E5F1] border-[#43E5F1]  sm:border-2 border-[1px]  second-shape ">
           <div className="h-[100%] w-[100%] bg-[#2d3e49] opacity-[99] second-shape2 flex justify-center items-center text-white ">
             <div className="flex flex-col md:gap-6 gap-3 items-center md:mt-20 mt-10">
@@ -23,6 +25,7 @@ const SecondShape = () => {
                 Thinking Of Disconnecting Your Discord Account ? Confirm To Log
                 Out.{" "}
               </span>
+              {/* buttons with clip path  */}
               <div className="md:m-10 m-5 flex justify-around md:gap-14 gap-7">
                 <div className="md:w-32 w-16 md:h-12 h-6 border-2 bg-red-500  border-red-500 button-shape">
                   <div className="w-[100%] h-[100%] button-shape bg-[#2d3e49] flex justify-center items-center ">
