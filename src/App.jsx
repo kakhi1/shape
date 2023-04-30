caa573f67b93d6c5852b69931933e7b46b2a4aa5
kakhi1 <kmchedluri@gmail.com>
2023-04-30 17:41:46 +0400

    third

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import FirstShape from "./FirstShape";
 import SecondShape from "./secondShape";
+import ThirdShape from "./ThirdShape";
 
 function App() {
   return (
     <>
+      <ThirdShape />
       {/* second shape    */}
       <SecondShape />
       {/* first shape  */}
---
+++ src/ThirdShape.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ThirdShape = () => {
+  return (
+    <div className="max-w-[1220px] max-h-[500px]  bg-yellow-400 ">
+      <dir className="flex items-center justify-center p-10">
+        <div className="w-screen h-[280px] bg-cover third-shape bg-no-repeat bg-white "></div>
+      </dir>
+    </div>
+  );
+};
+
+export default ThirdShape;
