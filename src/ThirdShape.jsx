6555ef2e30268d068909a11c308dca17eca62a7c
kakhi1 <kmchedluri@gmail.com>
2023-05-01 03:41:21 +0400

    background  img clip path

--- src/ThirdShape.jsx
+++ src/ThirdShape.jsx
@@ -2,10 +2,8 @@ import React from "react";
 
 const ThirdShape = () => {
   return (
-    <div className="max-w-[1220px] max-h-[500px]  bg-yellow-400 ">
-      <dir className="flex items-center justify-center p-10">
-        <div className="w-screen h-[280px] bg-cover third-shape bg-no-repeat bg-white "></div>
-      </dir>
+    <div className="max-w-[1320px] max-h-[400px] bg-yellow-400 flex justify-center items-center py-10 ">
+      <div className="lg:w-[1266px]  md:w-[1100px] sm:w-[1000px] w-[600px] lg:h-[279px] md:h-[180px] sm:h-[170px] h-[120px] bg-[url('./Vector.png')] lg:bg-auto bg-cover bg-no-repeat bg-center bg-white backgoundpath backgoundpath2 backgoundpathMobile"></div>
     </div>
   );
 };
