cc63efa25bfd9f1888f37b872a938c888e182d9d
kakhi1 <kmchedluri@gmail.com>
2023-05-01 19:05:10 +0400

    fix mobile screen

--- src/ThirdShape.jsx
+++ src/ThirdShape.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const ThirdShape = () => {
   return (
-    <div className="max-w-[1320px] max-h-[400px] bg-yellow-400 flex justify-center items-center py-10 ">
-      <div className="lg:w-[1266px]  md:w-[1100px] sm:w-[1000px] w-[600px] lg:h-[279px] md:h-[180px] sm:h-[170px] h-[120px] bg-[url('./Vector.png')] lg:bg-auto bg-cover bg-no-repeat bg-center bg-white backgoundpath backgoundpath2 backgoundpathMobile"></div>
+    <div className="max-w-[1320px] max-h-[400px] bg-yellow-400 flex justify-center items-center sm:py-10 py-1">
+      <div className="lg:w-[1266px]  md:w-[1100px] sm:w-[1000px] w-[353px] lg:h-[279px] md:h-[180px] sm:h-[170px] h-[280px] bg-[#43E5F1] border-[#43E5F1]  backgoundpathFirstDiv backgoundpath2FirstDiv  flex justify-start items-start backgoundpathMobileFirstDiv">
+        <div className="lg:w-[1266px]  md:w-[1100px] sm:w-[1000px] w-[353px] lg:h-[279px] md:h-[180px] sm:h-[170px] h-[278px]  border-[#43E5F1]  sm:border-[1px] border-0  sm:bg-[url('./Vector.png')]  sm:bg-local sm:bg-cover bg-right bg-no-repeat backgoundpath backgoundpath2 backgoundpathMobile"></div>
+      </div>
     </div>
   );
 };
